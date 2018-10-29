7e800d8e8ae93d990c8a9ba43e2e5f4d1c9533c1
sahanarula <sahiln123@gmail.com>
2018-10-29 18:33:23 -0400

    Zone centric init

---
+++ Assets/Scripts/ZoneCentric/Bound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bound : MonoBehaviour {
+	Bounds b;
+	// Use this for initialization
+	void Start () {
+		b = new Bounds(new Vector3(0, 0, 0), new Vector3(5, 5, 5));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ZoneCentric/VoronoiDiagram.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace ZoneCentric
+{
+	public class VoronoiDiagram : MonoBehaviour {
+		private SteamVR_TrackedObject _trackedObj;
+	
+		private SteamVR_Controller.Device Controller
+		{
+
+			get
+			{
+				return SteamVR_Controller.Input((int)_trackedObj.index);
+			}
+		}
+
+		void Start()
+		{
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube.transform.position = new Vector3(0, 0.5f, 0);
+		}
+	
+		void Awake()
+		{
+			_trackedObj = GetComponent<SteamVR_TrackedObject>();
+		}
+
+		void OnGUI ()
+		{
+			Rect textBox = new Rect(10, 120, 500, 100);
+			Vector3 pos = _trackedObj.transform.position;
+		
+			GUI.Label (textBox, "(X, Y, Z): (" + pos.x + " " + pos.y + " " + pos.z + ")");
+		}
+
+		// Update is called once per frame
+		void Update () {
+			
+			// Objects appear to init position 
+			if (Controller.GetHairTriggerDown())
+			{
+				Vector3 pos = _trackedObj.transform.position;
+				Debug.Log("(X, Y, Z): (" + pos.x + " " + pos.y + " " + pos.z + ")");
+			}
+
+		}
+	
+	}
+}
