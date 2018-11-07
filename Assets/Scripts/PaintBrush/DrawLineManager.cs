f293f4605f74a00dd22e8724f940c55abf09a1c5
sahanarula <sahiln123@gmail.com>
2018-11-07 13:11:05 -0500

    added paint brush for line drawing

--- Assets/Scripts/PaintBrush/DrawLineManager.cs
+++ Assets/Scripts/PaintBrush/DrawLineManager.cs
@@ -1,16 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
 using UnityEngine;
+using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class DrawLineManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public SteamVR_TrackedObject _trackedObj;
+	private LineRenderer currLine;
+	private int numClicks = 0;
 	
-	// Update is called once per frame
-	void Update () {
+	private SteamVR_Controller.Device Controller
+	{
+
+		get
+		{
+			return SteamVR_Controller.Input((int)_trackedObj.index);
+		}
+	}
 		
+	void Awake()
+	{
+		_trackedObj = GetComponent<SteamVR_TrackedObject>();				
+	}
+
+	void Update()
+	{
+		if (Controller.GetHairTriggerDown())
+		{
+			GameObject go = new GameObject();
+			currLine = go.AddComponent<LineRenderer>();
+			currLine.SetWidth(0.01f, 0.01f);
+			numClicks = 0;
+		} else if (Controller.GetHairTrigger())
+		{
+			currLine.SetVertexCount(numClicks + 1);
+			currLine.SetPosition(numClicks, _trackedObj.transform.position);
+			numClicks++;
+		}
 	}
 }
