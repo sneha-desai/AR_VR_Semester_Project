bc9327e926bc557f009c0ff68a200390c52d4a96
sahanarula <sahiln123@gmail.com>
2018-11-19 14:48:00 -0500

    Finishing camera movement,
    To Do: Finish camera movement, drawing tool scaling.

--- Assets/_Andre/_Scripts/SkyDraw.cs
+++ Assets/_Andre/_Scripts/SkyDraw.cs
@@ -52,14 +52,25 @@ public class SkyDraw : MonoBehaviour
                 DrawGameObject(PrefabArray[i], DrawPointTransform.position);
             }        
         }
-        if (Controller2.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
-        {
-            Vector2 axis = Controller2.GetAxis();
-            Vector3 forward = Vector3.Scale(new Vector3(0.1f+axis.x,0.1f*axis.y,0.1f), Camera.rotation.eulerAngles);
-            Debug.Log("Forward: " + forward);
-            Vector3 t = transform.InverseTransformDirection(Camera.transform.rotation.eulerAngles);
-//            transform.parent.Translate(forward);
-            transform.parent.transform.position += Camera.transform.forward * 2.0f * Time.deltaTime;
+//        if (Controller2.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+//        {
+//            Vector2 axis = Controller2.GetAxis();
+//            Vector3 forward = Vector3.Scale(new Vector3(0.1f+axis.x,0.1f*axis.y,0.1f), Camera.rotation.eulerAngles);
+//            Debug.Log("Forward: " + forward);
+//            Vector3 t = transform.InverseTransformDirection(Camera.transform.rotation.eulerAngles);
+////            transform.parent.Translate(forward);
+//            // Moving facing direction
+//            transform.parent.transform.position += Camera.transform.forward * 10.0f * Time.deltaTime * axis.y;
+//            transform.parent.transform.position += Camera.transform.right * 10.0f * Time.deltaTime * axis.x;
+//        }
+        if (Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {    
+//            Debug.Log("Trackpad1");
+            Vector2 axis2 = Controller.GetAxis();
+            DrawPointTransform.transform.localPosition = new Vector3(0, 0, (DrawPointTransform.position.z+axis2.y*.3f));
+            if (axis2.x > .8f) DrawPointTransform.localScale *= 1.1f;
+            if (axis2.x < -.8f) DrawPointTransform.localScale *= 0.9f;
+//            DrawPointTransform.localScale *= axis2.x > 0 ? 1.1f : 0.9f;
         }
     }
 
