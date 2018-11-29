2d6d8115be1303a9ded1265c6a168ca7223006c2
sahanarula <sahiln123@gmail.com>
2018-11-29 11:59:52 -0500

    Added Single Object mode and step mode

--- Assets/_Andre/_Scripts/SkyDrawVr.cs
+++ Assets/_Andre/_Scripts/SkyDrawVr.cs
@@ -7,6 +7,9 @@ namespace _Andre._Scripts
     {
         public Transform DrawPointTransform;
         public Transform[] PrefabArray;
+        public float Step = 0.0f;
+        public bool SingleMode = false;
+
         private float _cooldown = 1.0f;
 
         private SteamVR_TrackedObject _trackedObj;
@@ -19,6 +22,11 @@ namespace _Andre._Scripts
 
         private float _scaleSpeed = .1f;
         private float _distanceSpeed = .1f;
+        private Vector3 _lastPoint = Vector3.zero;
+
+        private int _prefabIndex = 0;
+        private Vector3 _localScale;
+        private float _localDistance = 10.0f;
 
         private SteamVR_Controller.Device Controller
         {
@@ -28,22 +36,33 @@ namespace _Andre._Scripts
         void Awake()
         {
             _trackedObj = GetComponent<SteamVR_TrackedObject>();
-            _nextPrefab = PrefabArray[0];
+            _nextPrefab = PrefabArray[_prefabIndex];
+            Increment();
             DrawPointTransform = Instantiate(_nextPrefab);
             DrawPointTransform.parent = _trackedObj.transform;
             DrawPointTransform.localPosition = new Vector3(0,0,10);
-            
+
+            _localScale = DrawPointTransform.localScale;
+
+        }
+
+        void Increment()
+        {
+            _prefabIndex++;
+            if (_prefabIndex >= PrefabArray.Length)
+            {
+                _prefabIndex = 0;
+            }
         }
 
         void Update()
         {
             _cooldown -= Time.deltaTime;
 
-            if (Controller.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+            if (Controller.GetPress(SteamVR_Controller.ButtonMask.Trigger) && SingleMode == false)
             {
                 if (_cooldown < 0)
                 {
-                    int i = Mathf.RoundToInt(Random.Range(0, PrefabArray.Length));
                     DrawGameObject(_nextPrefab, DrawPointTransform.position);
                 }
             }
@@ -57,14 +76,39 @@ namespace _Andre._Scripts
                     new Vector3(0, 0, (DrawPointTransform.localPosition.z + _axis.y * _scaleSpeed));
                 if (_axis.x > .8f) DrawPointTransform.localScale *= 1 +_distanceSpeed;
                 if (_axis.x < -.8f) DrawPointTransform.localScale *= 1 - _distanceSpeed;
+                _localScale = DrawPointTransform.localScale;
+                _localDistance = DrawPointTransform.transform.localPosition.z;
                 _newObjectScale = DrawPointTransform.localScale;
+            }
+            if (Controller.GetHairTriggerDown() && SingleMode)
+            {
+                if (_cooldown < 0)
+                {
+                    DrawGameObject(_nextPrefab, DrawPointTransform.position);
+                }
+            }
+
+            if (Controller.GetHairTriggerUp())
+            {
+                Debug.Log(gameObject.name + " Trigger Release");
             }
         }
 
         void DrawGameObject(Transform trans, Vector3 point)
         {
-            Transform o = Instantiate(trans, point, Quaternion.identity);
-            o.localScale = _newObjectScale;
+            
+            if (Vector3.Distance(point, _lastPoint) > Step || _lastPoint == Vector3.zero)
+            {
+                _lastPoint = point;
+                DrawPointTransform.parent = transform.parent.parent;
+                _nextPrefab = PrefabArray[_prefabIndex];
+                DrawPointTransform = Instantiate(_nextPrefab);
+                DrawPointTransform.parent = _trackedObj.transform;
+                DrawPointTransform.localScale = _localScale;
+                DrawPointTransform.localPosition = new Vector3(0,0,_localDistance);
+                Increment();
+            }
+            
         }
     }
 }
