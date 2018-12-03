babdcec7ed306b5cd8c8372c153109dc0dfa9deb
sahanarula <sahiln123@gmail.com>
2018-12-03 03:37:18 -0500

    separated zone finder

---
+++ Assets/ZoneFinder.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ZoneCentric;
+using UnityEngine;
+using Valve.VR;
+
+public class ZoneFinder : MonoBehaviour {
+    private SteamVR_TrackedObject _trackedObj;
+    //ZoneCentric
+    private static List<Zones> _zoneCentricZones;
+    private static int _currentLevelIndex;
+    private GameObject _reference;
+    private GameObject _cameraObject;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)_trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        _trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private int GetZone()
+    {
+        int zone = -1;
+        Vector3 clickedPoint = _cameraObject.transform.InverseTransformPoint(_trackedObj.transform.position);
+        //Debug.Log("controller point in world space " + _trackedObj.transform.position.x + " " + _trackedObj.transform.position.y + " " + _trackedObj.transform.position.z);
+        //Debug.Log("controller point in sphere space " + _trackedObj.transform.InverseTransformPoint(_trackedObj.transform.position).x*10f + " " + (_trackedObj.transform.InverseTransformPoint(_trackedObj.transform.position).y + 0.32f)*10f + " " + _trackedObj.transform.InverseTransformPoint(_trackedObj.transform.position).z*10f);
+
+        var polarPoint = PolarCoordinates.FromCartesian(clickedPoint);
+
+        var polar = PolarCoordinates.RadToDeg(polarPoint.Polar);
+        var elevation = PolarCoordinates.RadToDeg(polarPoint.Elevation);
+
+        Debug.Log("in sphere space as per function " + clickedPoint.x + " " + clickedPoint.y + " " + clickedPoint.z + " " + polar + " " + elevation);
+
+
+        //Vector3 clickedPoint = transform.InverseTransformPoint(_trackedObj.transform.position);
+
+        //            print("ClickedPoint: " + clickedPoint);
+        List<float> _subzones = _zoneCentricZones[_currentLevelIndex].SubZones;
+        for (int i = 0; i < _subzones.Count / 4; i++)
+        {
+            //Debug.Log("Checking subzone: " + i);
+            if (Interface.CheckPointInZone(clickedPoint, _zoneCentricZones, _currentLevelIndex, i))
+            {
+                zone = i;
+            }
+        }
+
+        //print("Cartesian ClickedPoint: " + clickedPoint + " Polar ClickedPoint: polar " + polar + " elevation " + elevation +" zone: " + zone);
+
+        return zone;
+    }
+
+    // Use this for initialization
+    void Start () {
+        //ZoneCentric Code
+        _zoneCentricZones = Interface.GetZones();
+        _currentLevelIndex = 1;
+        _reference = GameObject.Find("Reference");
+        _cameraObject = GameObject.Find("Camera (eye)");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Controller.GetPressDown(EVRButtonId.k_EButton_Grip))
+        {
+            int zone = GetZone();
+            Debug.Log("zone: " + zone);
+            PrefabArray = ZoneDictionary.GetPrefabArrayForZone(zone);
+        }
+    }
+}
---
+++ Assets/_Andre/_Scripts/InitZones.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using ZoneCentric;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+namespace _Andre._Scripts
+{
+    public class Zones
+    {
+        public int NumSubZones;
+        public List<float> SubZones;
+        public List<float> CenterOfSubZones;
+
+        public Zones()
+        {
+            SubZones = new List<float>();
+        }
+
+        public void AddSubZone(float minPolar, float maxPolar, float minElevation, float maxElevation)
+        {
+            SubZones.Add(minPolar);
+            SubZones.Add(maxPolar);
+            SubZones.Add(minElevation);
+            SubZones.Add(maxElevation);
+        }
+    }
+
+    public class Interface
+    {
+        private static List<Zones> _zones;
+        private static Interface _instance;
+
+        private Interface() { }
+
+        public static Interface Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Interface();
+                }
+                return _instance;
+            }
+        }
+
+        private static void InitZones()
+        {
+            _zones = new List<Zones> { new Zones() };
+            _zones[0].NumSubZones = 2;
+            _zones[0].AddSubZone(0f, 180f, -90f, 90f);
+            _zones[0].AddSubZone(180f, 360f, -90f, 90f);
+
+            _zones.Add(new Zones());
+            _zones[1].NumSubZones = 4;
+            _zones[1].AddSubZone(0f, 180f, -90f, 0f);
+            _zones[1].AddSubZone(0f, 180f, 0f, 90f);
+            _zones[1].AddSubZone(180f, 360f, -90f, 0f);
+            _zones[1].AddSubZone(180f, 360f, 0f, 90f);
+        }
+
+        public static List<Zones> GetZones()
+        {
+            InitZones();
+            return _zones;
+        }
+
+
+        public static bool CheckPointInZone(Vector3 cartesianPoint, List<Zones> zoneCentricZones, int currentLevelIndex, int currentSubZone)
+        {
+            List<float> subzones = zoneCentricZones[currentLevelIndex].SubZones;
+            float minPolar = subzones[4 * currentSubZone + 0];
+            float maxPolar = subzones[4 * currentSubZone + 1];
+            float minElevation = subzones[4 * currentSubZone + 2];
+            float maxElevation = subzones[4 * currentSubZone + 3];
+
+
+            var polarPoint = PolarCoordinates.FromCartesian(cartesianPoint);
+
+            var polar = PolarCoordinates.RadToDeg(polarPoint.Polar);
+            var elevation = PolarCoordinates.RadToDeg(polarPoint.Elevation);
+
+
+            // Condition when subzone traverses from fourth quadrant to first quadrant;
+            if (maxPolar < minPolar)
+            {
+                minPolar -= 360f;
+                if (polar >= 0f && polar <= maxPolar)
+                    return (elevation >= minElevation) && (elevation <= maxElevation);
+
+                polar -= 360f;
+                if (polar >= minPolar && polar <= 0f)
+                {
+                    return (elevation >= minElevation) && (elevation <= maxElevation);
+                }
+                return false;
+            }
+
+            if ((polar >= minPolar) && (polar <= maxPolar) && (elevation >= minElevation) && (elevation <= maxElevation))
+            {
+                // Debug.Log("polar " + polar + " elevation " + elevation + " minPolar " + minPolar + " maxpolar " + maxPolar + " minElevation " + minElevation + " maxelevation " + maxElevation);
+            }
+
+
+            return (polar >= minPolar) && (polar <= maxPolar) && (elevation >= minElevation) && (elevation <= maxElevation);
+        }
+    }
+}
